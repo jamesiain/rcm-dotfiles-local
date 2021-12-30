ba60add20c900b1f6bcfadd9fab3e9a8440a05f1
James MacMillan <jamesiain@me.com>
2021-12-29 20:17:28 -0500

    Use gitstatus for prompt
    zsh-git-prompt is being replaced by a better and faster system, which is
    also available through the Homebrew system on MacOS:
        # brew install romkatv/gitstatus/gitstatus
        # echo "source $(brew --prefix)/opt/gitstatus/gitstatus.prompt.zsh" >>! ~/.zshrc
    
    More details on gitstatus is available here:
        https://github.com/romkatv/gitstatus

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,57 +1,21 @@
 setopt prompt_subst     # enable command substition in prompt
 
 zle_highlight=(default:fg=cyan)     # greater visibility for typed commands
-PROMPT='%F{green}%m%f %F{yellow}%~%f %# '
 
-# Use my customized fancy zsh-git-prompt
-[[ -d "${HOME}/.zsh/plugins/zsh-git-prompt" ]] && \
-  source "${HOME}/.zsh/plugins/zsh-git-prompt/zsh-git-prompt.sh"
+function zle-line-init zle-keymap-select {
+    # Git status information for shell prompt
+    [[ -d $(brew --prefix)/opt/gitstatus ]] && \
+      source $(brew --prefix)/opt/gitstatus/gitstatus.prompt.zsh
 
-function zle-keymap-select redraw-prompt {
+    PROMPT='%F{green}%n@%m%f %F{yellow}%3~%f %# '
+
     NORMAL_MODE="%{$fg[black]%} %{$bg[yellow]%} NORMAL %{$reset_color%}"
     VI_RPROMPT="${${KEYMAP/vicmd/$NORMAL_MODE}/(main|viins)/}"
 
-    RPROMPT="${VI_RPROMPT}"
+    RPROMPT="${VI_RPROMPT} ${GITSTATUS_PROMPT}"
 
-    # read from git status from temp file
-    [[ -r /tmp/zsh_prompt_$$ ]] && \
-        RPROMPT+=" $(cat /tmp/zsh_prompt_$$)"
-
-    # redisplay
-    zle && zle reset-prompt
+    zle reset-prompt    # redisplay
 }
 
+zle -N zle-line-init
 zle -N zle-keymap-select
-
-ASYNC_PROC=0
-function precmd() {
-    function async() {
-        # guard against missing $(git_super_status)
-        [[ ! -f "${HOME}/.zsh/plugins/zsh-git-prompt//zsh-git-prompt.sh" ]] && \
-            return
-
-        # save to temp file
-        printf "%s" "$(git_super_status)" > "/tmp/zsh_prompt_$$"
-
-        # signal parent
-        kill -s USR1 $$
-    }
-
-    # do not clear RPROMPT, let it persist
-
-    # kill child if necessary
-    if [[ "${ASYNC_PROC}" != 0 ]]; then
-        kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
-    fi
-
-    # start background computation
-    async &!
-    ASYNC_PROC=$!
-}
-
-function TRAPUSR1() {
-    # reset proc number
-    ASYNC_PROC=0
-
-    redraw-prompt
-}
